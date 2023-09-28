b9b0046af94e6f5f31bc6199d1e94982edff590b
Noel Pereira <76896172+noelp2500@users.noreply.github.com>
2023-09-27 22:10:59 -0700

    first commit

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,42 @@
+import { useProgress } from "@react-three/drei";
+import { usePlay } from "../contexts/Play";
+
+export const Overlay = () => {
+  const { progress } = useProgress();
+  const { play, end, setPlay, hasScroll } = usePlay();
+  return (
+    <div
+      className={`overlay ${play ? "overlay--disable" : ""}
+    ${hasScroll ? "overlay--scrolled" : ""}`}
+    >
+      <div
+        className={`loader ${progress === 100 ? "loader--disappear" : ""}`}
+      />
+      {progress === 100 && (
+        <div className={`intro ${play ? "intro--disappear" : ""}`}>
+          <h1 className="logo">
+            Noel Pereira
+            <div className="spinner">
+              <div className="spinner__image" />
+            </div>
+          </h1>
+          <p className="intro__scroll">Scroll to begin the journey</p>
+          <button
+            className="explore"
+            onClick={() => {
+              setPlay(true);
+            }}
+          >
+            Explore
+          </button>
+        </div>
+      )}
+      <div className={`outro ${end ? "outro--appear" : ""}`}>
+        <p className="outro__text">
+          Have a good one, you. Contact me via npereira4@ucmerced.edu, for those
+          of you who want to know more!
+        </p>
+      </div>
+    </div>
+  );
+};
